b72449ec9391886497e25907404eb22f63a2a074
Doug Ryan <ryan_djr@yahoo.com>
2021-11-02 17:23:33 -0700

    Add project files.

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,19 @@
+//*********************************************************************
+// Filename:
+// Author:
+// Date:
+// Class:
+// Assignment:
+// Purpose:
+// Hours:
+//*********************************************************************
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	cout << "Hello GitHub" << endl
+		<< "You successfully cloned the repository" << endl;
+
+	return EXIT_SUCCESS;
+}
